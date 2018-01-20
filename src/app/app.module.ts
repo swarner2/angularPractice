8340316e80d26731955732344cb04411d8efa657
Samuel Warner <samuelwarner@Samuels-MacBook-Air.local>
2018-01-20 15:36:24 -0500

    generated routing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { NewThingComponent } from "./new-thing/new-thing.component";
+import { AppRoutingModule } from ".//app-routing.module";
 
 @NgModule({
   declarations: [AppComponent, NewThingComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
