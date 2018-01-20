3fd9b9e43b177ab45de5d7e8a34ad3a2595462f2
Samuel Warner <samuelwarner@Samuels-MacBook-Air.local>
2018-01-20 10:48:01 -0500

    added component to main app

--- src/app/new-thing/new-thing.component.ts
+++ src/app/new-thing/new-thing.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-new-thing',
-  templateUrl: './new-thing.component.html',
-  styleUrls: ['./new-thing.component.css']
+  selector: "app-new-thing",
+  templateUrl: "./new-thing.component.html",
+  styleUrls: ["./new-thing.component.css"]
 })
 export class NewThingComponent implements OnInit {
+  name = "Bob";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
